126a9e832c22659f7ac4acadc9d45f7d18c25fb2
DARKZIPPER <95547664+DARKZIPPER@users.noreply.github.com>
2021-12-05 00:37:14 +0300

    HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -13,6 +13,11 @@ namespace HelloWorld
             Console.WriteLine("Hello World!!!");
 
             Console.ReadKey();
+
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
         }
     }
 }
